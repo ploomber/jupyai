cc0b850abffd4724eae7a14a079294e65eb0e303
Eduardo Blancas <github@blancas.io>
2023-12-24 10:47:41 -0600

    updates server code

--- jupyai/handlers.py
+++ jupyai/handlers.py
@@ -4,21 +4,24 @@ from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
+from jupyai import autocomplete
+
+
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
-    def get(self):
-        self.finish(json.dumps({
-            "data": "This is /jupyai/get-example endpoint!"
-        }))
+    def post(self):
+        body = self.get_json_body()
+
+        output = autocomplete.autocomplete(body)
+        self.finish(json.dumps({"data": output}))
 
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "jupyai", "get-example")
-    handlers = [(route_pattern, RouteHandler)]
+    handlers = [(url_path_join(base_url, "jupyai", "autocomplete"), RouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
