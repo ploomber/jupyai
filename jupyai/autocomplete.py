33fafb8ec297245cee149bd80e8ee3828ab3cbaa
Eduardo Blancas <github@blancas.io>
2023-12-26 18:07:26 -0600

    general improvements and tests

--- jupyai/autocomplete.py
+++ jupyai/autocomplete.py
@@ -1,8 +1,29 @@
+import logging
 from enum import Enum
+from os import environ
 
 from openai import OpenAI
 
 from jupyai import exceptions
+
+logger = logging.getLogger(__name__)
+
+if environ.get("JUPYAI_DEBUG"):
+    # Set the logger's level to DEBUG
+    logger.setLevel(logging.DEBUG)
+
+    # Create a console handler with level DEBUG
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and add it to the handler
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
 
 
 class TaskType(Enum):
@@ -13,19 +34,24 @@ class TaskType(Enum):
 
 prompt_generate_code = """
 You are a helpful Python code generator. Your job is to generate Python code based on
-a command given by the user. You should limit yourself to return Python code, that is,
+a command given by the user (wrapped between [COMMAND] [/COMMAND] tags).
+
+You should limit yourself to return Python code, that is,
 no markdown and no raw text (Python commends are fine). Your output must be runnable
 Python code (comments ok), that is, it should not contain [CODE] or [/CODE] tags.
 User's code might refer to existing code. Code that exist in previous lines will
 be given between [CODE_PREV] and [/CODE_PREV] tags. Code that exist in following
-lines will be given between [CODE_POST] and [/CODE_POST] tags. When generating output,
-you should only include new code, that is, do not repeat code within [CODE_PREV] and
-[/CODE_PREV] tags or within [CODE_POST] and [/CODE_POST] tags.
+lines will be given between [CODE_POST] and [/CODE_POST] tags.
+
+When generating output, you must re-use existing variables and only generate new code,
+your output should not contain code that already exists in previous lines.
 """
 
 prompt_fix_code = """
 You are a helpful Python code fixer. Your job is to fix Python code based on
-a command given by the user. You should limit yourself to return a fixed
+a command given by the user (wrapped between [COMMAND] [/COMMAND] tags).
+
+You should limit yourself to return a fixed
 Python code, that is, no markdown and no raw text (Python commends are fine). The
 user code will be given
 between [CODE] and [/CODE] tags. While errors produced by the code will be given
@@ -34,15 +60,18 @@ be runnable Python code (comments ok), that is, it should not contain
 [CODE] or [/CODE] tags.
 User's code might refer to existing code. Code that exist in previous lines will
 be given between [CODE_PREV] and [/CODE_PREV] tags. Code that exist in following
-lines will be given between [CODE_POST] and [/CODE_POST] tags.  When generating output,
-you should only include new code, that is, do not repeat code within [CODE_PREV] and
-[/CODE_PREV] tags or within [CODE_POST] and [/CODE_POST] tags.
+lines will be given between [CODE_POST] and [/CODE_POST] tags.
+
+When generating output, you must re-use existing variables and only generate new code,
+your output should not contain code that already exists in previous lines.
 """
 
 
 prompt_modify_code = """
 You are a helpful Python code modifier. Your job is to modify Python code based on
-a command given by the user. You should limit yourself to return an updated
+a command given by the user  (wrapped between [COMMAND] [/COMMAND] tags).
+
+You should limit yourself to return an updated
 Python code, that is, no markdown and no raw text (Python commends are fine). The
 user code will be given between [CODE] and [/CODE] tags. While the user command
 will be given between [COMMAND] and [/COMMAND] tags. You must update the code
@@ -50,29 +79,28 @@ based on the command. Your output must be runnable
 Python code (comments ok), that is, it should not contain [CODE] or [/CODE] tags.
 User's code might refer to existing code. Code that exist in previous lines will
 be given between [CODE_PREV] and [/CODE_PREV] tags. Code that exist in following
-lines will be given between [CODE_POST] and [/CODE_POST] tags.  When generating output,
-you should only include new code, that is, do not repeat code within [CODE_PREV] and
-[/CODE_PREV] tags or within [CODE_POST] and [/CODE_POST] tags.
+lines will be given between [CODE_POST] and [/CODE_POST] tags. 
+
+When generating output, you must re-use existing variables and only generate new code,
+your output should not contain code that already exists in previous lines.
 """
 
 
-# TODO: pass context from previous cells
-# TODO: sanitize output in case it's wrapped into ```
-# TODO: handle cases when there is nothing to do:
-# - empty cell
-# - cell with no command and no errors
-
-
 def _parse_cell(cell):
-    source_lines = cell["source"].split("\n")
+    cell_source = cell["source"].strip()
+
+    if not cell_source.replace("#", "").strip():
+        raise exceptions.BadInputException("Cell cannot be empty")
+
+    source_lines = cell_source.split("\n")
 
     # find first line that's not a comment
-    for i, line in enumerate(source_lines):
-        if not line.startswith("#"):
+    for i, line in enumerate(source_lines, start=1):
+        if not line.lstrip().startswith("#"):
             break
 
     # get the top comment (user's command)
-    command = "\n".join(line[1:] for line in source_lines[:i])
+    command = " ".join(line.strip()[1:].strip() for line in source_lines[:i])
 
     # get remaining lines
     code = "\n".join(source_lines[i:])
@@ -105,14 +133,14 @@ def parse_command(cell, sources):
     code_post = [cell["source"] for cell in sources[i + 1 :]]
 
     if errors:
-        return TaskType.FIX_CODE, Command(command) + Code(code) + str(
-            CodePrev(code_prev)
-        ) + str(CodePost(code_post)) + str(Errors(errors))
+        return TaskType.FIX_CODE, Command(command) + str(CodePrev(code_prev)) + str(
+            CodePost(code_post)
+        ) + str(Errors(errors))
 
     if code:
-        return TaskType.MODIFY_CODE, Command(command) + Code(code) + str(
-            CodePrev(code_prev)
-        ) + str(CodePost(code_post))
+        return TaskType.MODIFY_CODE, Command(command) + str(CodePrev(code_prev)) + str(
+            CodePost(code_post)
+        )
 
     return TaskType.GENERATE_CODE, Command(command) + str(CodePrev(code_prev)) + str(
         CodePost(code_post)
@@ -132,18 +160,6 @@ class Command(Component):
         return f"""
 [COMMAND]
 {self.command}
-[/COMMAND]
-"""
-
-
-class Code(Component):
-    def __init__(self, code: str) -> None:
-        self.code = code
-
-    def __str__(self) -> str:
-        return f"""
-[COMMAND]
-{self.code}
 [/COMMAND]
 """
 
@@ -194,19 +210,6 @@ class Errors(Component):
         return "\n".join([str(Error(error)) for error in self.errors])
 
 
-def autocomplete(cell, sources):
-    task_type, command = parse_command(cell, sources)
-
-    if task_type == TaskType.GENERATE_CODE:
-        return run_task(prompt_generate_code, command)
-    elif task_type == TaskType.FIX_CODE:
-        return run_task(prompt_fix_code, command)
-    elif task_type == TaskType.MODIFY_CODE:
-        return run_task(prompt_modify_code, command)
-    else:
-        raise ValueError("Unknown task type: " + task_type)
-
-
 def run_task(system_prompt, command):
     try:
         client = OpenAI()
@@ -224,6 +227,49 @@ def run_task(system_prompt, command):
         ],
     )
 
-    output = completion.choices[0].message.content
+    output = remove_code_fences(completion.choices[0].message.content)
 
     return output
+
+
+def remove_code_fences(code: str) -> str:
+    PREFIX, SUFFIX = "```python", "```"
+
+    code = code.strip()
+
+    if code.startswith(PREFIX) and code.endswith(SUFFIX):
+        code = code[len(PREFIX) : -len(SUFFIX)]
+
+    return code.strip()
+
+
+def autocomplete(cell, sources):
+    """
+    Public API for autocompletion. This is the function that gets called when users
+    trigger autocompletion from JupyterLab
+
+    Parameters
+    ----------
+    cell : dict
+        The cell to autocomplete. It must contain a `source` key with the source code
+        and an `id` key with the cell's id.
+
+    sources : list[dict]
+        The list of all cells in the notebook
+    """
+    if not sources:
+        raise ValueError("Sources cannot be empty")
+
+    task_type, command = parse_command(cell, sources)
+
+    if task_type == TaskType.GENERATE_CODE:
+        prompt = prompt_generate_code
+    elif task_type == TaskType.FIX_CODE:
+        prompt = prompt_fix_code
+    elif task_type == TaskType.MODIFY_CODE:
+        prompt = prompt_modify_code
+    else:
+        raise ValueError("Unknown task type: " + task_type)
+
+    logger.debug("Running task %s\nPrompt:\n%sCommand:\n%s", task_type, prompt, command)
+    return run_task(prompt, command)
---
+++ jupyai/tests/test_autocomplete.py
@@ -0,0 +1,72 @@
+from unittest.mock import Mock
+
+import pytest
+
+from jupyai import autocomplete
+
+
+@pytest.mark.parametrize(
+    "cell, expected_command, expected_code, expected_errors",
+    [
+        [
+            {"source": "# generate some code", "id": "first"},
+            "generate some code",
+            "",
+            [],
+        ],
+        [
+            {
+                "source": "# generate some code\n # that does something cool",
+                "id": "first",
+            },
+            "generate some code that does something cool",
+            "",
+            [],
+        ],
+    ],
+)
+def test_parse_cell(cell, expected_command, expected_code, expected_errors):
+    command, code, errors = autocomplete._parse_cell(cell)
+
+    assert command == expected_command
+    assert code == expected_code
+    assert errors == expected_errors
+
+
+@pytest.mark.parametrize(
+    "openai_response, expected_output",
+    [
+        ["x + y", "x + y"],
+        ["\n```python\nx + y\n```", "x + y"],
+    ],
+)
+def test_generate_code(monkeypatch, openai_response, expected_output):
+    mock_client = Mock()
+    mock_completion = Mock(choices=[Mock(message=Mock(content=openai_response))])
+    mock_client.chat.completions.create.return_value = mock_completion
+
+    monkeypatch.setattr(autocomplete, "OpenAI", lambda: mock_client)
+
+    response = autocomplete.autocomplete(
+        cell={
+            "source": "# generate code to add x and y",
+            "id": "third",
+        },
+        sources=[
+            {
+                "source": "x = 1",
+                "id": "first",
+                "source": "y = 1",
+                "id": "second",
+                "source": "# generate code to add x and y",
+                "id": "third",
+            }
+        ],
+    )
+
+    assert response == expected_output
+    msg_system, msg_user = mock_client.chat.completions.create.call_args.kwargs[
+        "messages"
+    ]
+    assert "code generator" in msg_system["content"]
+    assert "generate code to add x and y" in msg_user["content"]
